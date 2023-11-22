5779a8fa5287f41f0344c38645f6fb7646bb0e40
Aileck <61742408+Aileck@users.noreply.github.com>
2023-11-22 21:57:30 +0100

    chore: include tiny_gltf

---
+++ Engine/Source/Mesh.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define TINYGLTF_NO_STB_IMAGE_WRITE
+#define TINYGLTF_NO_STB_IMAGE
+#define TINYGLTF_NO_EXTERNAL_IMAGE
+#define TINYGLTF_IMPLEMENTATION /* Only in one of the includes */
+#include "./include/tinygltf/tiny_gltf.h"
+class Mesh
+{
+};
+
