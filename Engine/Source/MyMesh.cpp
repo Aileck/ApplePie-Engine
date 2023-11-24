383df7e6bdfb6733f6bbaf7368d5a7319a4fd3cf
Aileck <61742408+Aileck@users.noreply.github.com>
2023-11-25 00:06:51 +0100

    feat: add mesh

--- Engine/Source/MyMesh.cpp
+++ Engine/Source/MyMesh.cpp
@@ -1,9 +1,147 @@
 #include "MyMesh.h"
+#include "SDL.h"
+#include "GL\glew.h"
 
+#include "Application.h"
+#include "ModuleOpenGL.h"
+#include "ModuleProgram.h"
+#include "./include/MathGeoLib/Math/MathAll.h"
+
+
+
+#define PROGRAM App->GetProgram()->program
 MyMesh::MyMesh()
 {
 }
 
 MyMesh::~MyMesh()
 {
+}
+
+
+// Create Vertex Array Object (VAO)
+void MyMesh::LoadVAO(const Model& model, const Mesh& mesh, const Primitive& primitive)
+{
+	const auto& itPos = primitive.attributes.find("POSITION");
+	if (itPos != primitive.attributes.end())
+	{
+		const Accessor& posAcc = model.accessors[itPos->second];
+		SDL_assert(posAcc.type == TINYGLTF_TYPE_VEC3);
+		SDL_assert(posAcc.componentType == GL_FLOAT);
+		const BufferView& posView = model.bufferViews[posAcc.bufferView];
+		const Buffer& posBuffer = model.buffers[posView.buffer];
+		const unsigned char* bufferPos = &(posBuffer.data[posAcc.byteOffset + posView.byteOffset]);
+
+		glGenBuffers(1, &m_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * posAcc.count, nullptr, GL_STATIC_DRAW);
+		float3* ptr = reinterpret_cast<float3*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY));
+		for (size_t i = 0; i < posAcc.count; ++i)
+		{
+			ptr[i] = *reinterpret_cast<const float3*>(bufferPos);
+			bufferPos += posView.byteStride;
+		}
+		glUnmapBuffer(GL_ARRAY_BUFFER);
+
+		vertexCount = posAcc.count;
+	}
+}
+
+// Load index data from GLTF model
+void MyMesh::LoadEBO(const Model& model, const Mesh& mesh, const Primitive& primitive)
+{
+	if (primitive.indices >= 0)
+	{
+		const Accessor& indAcc = model.accessors[primitive.indices];
+		const BufferView& indView = model.bufferViews[indAcc.bufferView];
+		glGenBuffers(1, &m_ebo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indAcc.count, nullptr, GL_STATIC_DRAW);
+		unsigned int* ptr = reinterpret_cast<unsigned int*>(glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY));
+
+		const unsigned char* buffer = &(model.buffers[indView.buffer].data[indAcc.byteOffset +
+			indView.byteOffset]);
+
+		if (indAcc.componentType == TINYGLTF_PARAMETER_TYPE_UNSIGNED_INT)
+		{
+			const uint32_t* bufferInd = reinterpret_cast<const uint32_t*>(buffer);
+			for (uint32_t i = 0; i < primitive.indices; ++i) ptr[i] = bufferInd[i];
+		}
+		/* TODO indAcc.componentType == TINYGLTF_PARAMETER_TYPE_UNSIGNED_SHORT*/
+		/* TODO indAcc.componentType == TINYGLTF_PARAMETER_TYPE_UNSIGNED_BYTE*/
+		glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
+
+		vertexCount = indAcc.count;
+	}
+}
+
+void MyMesh::CreateVAO()
+{
+	glGenVertexArrays(1, &m_vao);
+	glBindVertexArray(m_vao);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vao);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)(sizeof(float) * 3 * vertexCount));
+
+	glBindVertexArray(0);
+}
+//void MyMesh::Render()
+//{
+//	glUseProgram(App->GetProgram()->program);
+//	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+//	glEnableVertexAttribArray(0);
+//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3 + sizeof(float) * 2, (void*)0);
+//	glEnableVertexAttribArray(1);
+//	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 3 + sizeof(float) * 2, (void*)(sizeof(float) * 3));
+//	glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+//}
+
+// Render with EBO
+void MyMesh::Render()
+{
+	glUseProgram(PROGRAM);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)(sizeof(float) * 3 * vertexCount));
+	glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
+}
+
+void MyMesh::RenderInterleavedArrays() // good for static meshes
+{
+	glUseProgram(PROGRAM);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3 + sizeof(float) * 2, (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 3 + sizeof(float) * 2, (void*)(sizeof(float) * 3));
+	glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+
+}
+
+void MyMesh::RenderSeparatedArrays() //  good for dynamic meshes writing
+{
+	glUseProgram(PROGRAM);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)(sizeof(float) * 3 * vertexCount));
+	glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+}
+
+
+
+void MyMesh::Draw(const std::vector<unsigned>& textures)
+{
+	glUseProgram(App->GetProgram()->program);
+	glActiveTexture(GL_TEXTURE0);
+	//glBindTexture(GL_TEXTURE_2D, textures[materialIndex]);
+	glUniform1i(glGetUniformLocation(App->GetProgram()->program, "diffuse"), 0);
+	glBindVertexArray(m_vao);
+	glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, nullptr);
 }
--- Engine/Source/MyMesh.h
+++ Engine/Source/MyMesh.h
@@ -1,16 +1,30 @@
 #pragma once
+#define TINYGLTF_NO_STB_IMAGE_WRITE
+#define TINYGLTF_NO_STB_IMAGE
+#define TINYGLTF_NO_EXTERNAL_IMAGE
+#include "./include/tinygltf/tiny_gltf.h"
 
-
-
+using namespace tinygltf;
 class MyMesh
 {
 public:
 	MyMesh();
 	~MyMesh();
+
+	void CreateVAO();
+
+	void LoadVAO(const tinygltf::Model& model, const tinygltf::Mesh& mesh, const tinygltf::Primitive& primitive);
+	void LoadEBO(const tinygltf::Model& model, const tinygltf::Mesh& mesh, const tinygltf::Primitive& primitive);
+
+	void Render();
+	void RenderInterleavedArrays();
+	void RenderSeparatedArrays();
+	void Draw(const std::vector<unsigned>& textures);
 private:
 	unsigned m_vbo = 0;
 	unsigned m_vao = 0;
 	unsigned m_ebo = 0;
+	size_t vertexCount = 0;
+	size_t indexCount = 0;
 
 };
-
